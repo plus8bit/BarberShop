e0821eb790f2d77b892a06c251703849748d40b3
Nick <plus0bit@gmail.com>
2018-11-09 19:18:10 +0300

    Keep values on post

--- app.rb
+++ app.rb
@@ -25,16 +25,17 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
-	c = Client.new params[:client]
-	if c.save
+	@c = Client.new params[:client]
+	if @c.save
 		erb "Thank you for your appointment with us."
 	else
-		@error = c.errors.full_messages.first
-		erb :visit
+		@error = @c.errors.full_messages.first
+		erb :visit 
 	end
 end
 
